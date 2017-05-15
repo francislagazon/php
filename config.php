b16d47e0f58610fafea1c7d99d8c8f3472b966f0
francislagazon <francislagazon@yahoo.com>
2017-05-15 13:57:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ Author: Francis Lagazon
 */
 
 /* DATABASE CONFIG */
-define("DB_HOST", "localhost");
-define("DB_NAME", "forum");
-define("DB_USER", "root");
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
 define("DB_PASS", "");
 
 /* PATHS CONFIG */
